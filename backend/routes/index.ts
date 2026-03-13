50172a72e6c83a7353b23f8c138e6d6514bc2828
LuizGomes56 <luizgustavodgomes@gmail.com>
2026-03-13 14:33:52 -0400

    Project files setup

---
+++ backend/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post("/login", /* Controller.login */);
+router.get("/logout", /* Controller.logout */);
+
+export default router;
