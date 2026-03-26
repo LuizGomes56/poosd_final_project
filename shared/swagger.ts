03706f1a11bb2ebecf687ec92864fc873440a7ac
Giovana Medeiros <giovana05062003@gmail.com>
2026-03-26 19:09:36 -0400

    Add SwaggerDocs type to shared file

--- shared/swagger.ts
+++ shared/swagger.ts
@@ -1,2 +1,50 @@
 export type HttpStatus = number;
-export type SwaggerDocs = any;
+
+export type SwaggerDocs = {
+    "users/login": {
+        method: "POST";
+        output:
+            | {
+                  ok: boolean;
+                  status: HttpStatus;
+                  message: string;
+                  body: undefined;
+              }
+            | {
+                  ok: boolean;
+                  status: HttpStatus;
+                  message: string;
+                  body: {
+                      token: string;
+                  };
+              };
+        input: {
+            email: string;
+            password: string;
+        };
+    };
+
+    "users/logout": {
+        method: "GET";
+        output: {
+            ok: boolean;
+            status: HttpStatus;
+            message: string;
+        };
+        input: undefined;
+    };
+
+    "users/register": {
+        method: "POST";
+        output: {
+            ok: boolean;
+            status: HttpStatus;
+            message: string;
+        };
+        input: {
+            full_name: string;
+            email: string;
+            password: string;
+        };
+    };
+};
