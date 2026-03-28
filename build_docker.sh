207658f5c539ffd5530ba98e04634bcfeb7cb9f1
Jashua Sauri <Jashuasauri@gmail.com>
2026-03-28 18:27:14 -0400

    Added new dockerbuild for frontend and is served by nginx

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+docker image rm poosdproject/backendapp
 docker build -f backend/docker . -t poosdproject/backendapp
+docker image rm poosdproject/frontendapp
 docker build -f frontend/docker . -t poosdproject/frontendapp
