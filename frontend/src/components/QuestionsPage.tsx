a5360e01bb4e7cecf409f3def26322de98c91249
JYSCN <Jyssauco1@gmail.com>
2026-04-01 01:07:28 -0400

    Added questions page utilizing new popup features and modified new components to accept custom styling

--- frontend/src/components/QuestionsPage.tsx
+++ frontend/src/components/QuestionsPage.tsx
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Table from "./Table";
 import { translate, type ActionFn, type SetState } from "../consts";
 import { api } from "../utils/request";
-import QuestionEditPopup from "./QuestionEdit";
-import QuestionEditPage from "./QuestionEdit";
+import FormBuilder from "../forms/FormBuilder";
+import FormTextField from "../forms/FormTextField";
+import FormButton from "../forms/FormButton";
+
+
 type TableEntry = {
     value: string,
     style?: string
@@ -20,6 +23,12 @@ function requestQuestions(){
     return null as NullableQuestionTable
 } 
 
+function questionCrudOperations(id: any) {
+    return {id: id, question: "Who are you?", answers: [{id: 0, answer: "testing one answer"}, {id: 1, answer: "testing two answers"}, {id: 2, answer: "testing three answers"}, {id: 3, answer: "testing four answers"},
+        {id: 4, answer: "testing five answers"}
+    ]
+    }
+}
 const QuestionDisplay = ({questions, setAction}
     :
     {
@@ -72,23 +81,62 @@ const QuestionDisplay = ({questions, setAction}
 }
 
 const QuestionsPage = () => {
+    let individualQuestion = questionCrudOperations("");
      const [_action, setAction] = useState<ActionFn>(null);
+    const [show, setShow] = useState(false);
+    const [question, setQuestion] = useState(individualQuestion.question);
+    const [answers, setAnswers] = useState(individualQuestion.answers);
      console.log(_action)
     let questions = requestQuestions();
-    if(_action != null && _action.mode == "UPDATE") {
-        return (
-            <div>
-                <QuestionEditPage id={_action.id}/>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div>
-                <QuestionDisplay questions={questions} setAction={setAction}/>
-            </div>
-        )
-    }
+    useEffect(() => {
+        console.log("Effect has been executed")
+        if(_action?.mode == "UPDATE") {
+            setShow(true);
+        }
+    })
+
+    answers.map((answer, index)=> {console.log(`answer: ${answer}, index: ${index}`)})
+
+    return (
+        <div>
+            <div className="">
+            <FormBuilder
+            show={show}
+            setShow={setShow}>
+                <FormTextField id="Question" value={question} setValue={setQuestion} title="Question" titleStyle={"text-black"}/>
+                <div className="overflow-y-scroll">
+                {answers.map((answer, index) => (
+                    <>
+                    <FormTextField id={String(index)} value={answer.answer} setValue={(newValue) => {
+                        let localAnswers = structuredClone(answers);
+                        localAnswers[index].answer = newValue;
+                        setAnswers(localAnswers);
+                    }} title={"Question #" + (index+1)} titleStyle={"text-black"}/>
+                    <FormButton type="button" text="delete"/>
+                    </>
+                ))
+
+                }
+                </div>
+                <FormButton type="submit" text="update"/>
+                <FormButton type="button" text="Add Question"/>
+            </FormBuilder>
+
+            
+            {/* <Field name="Question" value={question} hook={setQuestion} textStyle="text-white" spanStyle="font-medium text-white text-lg border-b-2 pb-1 border-b-white"/>
+            {answers.map((answer) => (
+                <Field name="Answer" value={answer.answer} hook={(newValue) => { 
+                    let localAnswers = structuredClone(answers);
+                    localAnswers[answer.id].answer = newValue;
+                    setAnswers(localAnswers);
+                }} textStyle="text-white" spanStyle="font-medium text-white text-lg border-b-2 pb-1 border-b-white"/>
+            ))} */}
+        </div>
+    )
+            <QuestionDisplay questions={questions} setAction={setAction}/>
+        </div>
+    )
+    
 }
 
 export default QuestionsPage;
--- frontend/src/forms/FormTextField.tsx
+++ frontend/src/forms/FormTextField.tsx
@@ -1,26 +1,34 @@
 import { useRef } from "react";
 import type { SetState } from "../consts";
-
+//Honestly since I am passing so many style strings 
+//Perhaps it may be better to change this to a style
+//object rather than doing this individually
 const FormTextField = ({
     id,
     title,
     placeholder = "",
     value,
     setValue,
-    maxLength = 40
+    maxLength = 40,
+    titleStyle,
+    placeholderStyle,
+    divStyle
 }: {
     id: string,
     value: any,
     setValue: SetState<any>,
     title: React.ReactNode,
     placeholder?: string,
-    maxLength?: number
+    maxLength?: number,
+    titleStyle?: string,
+    placeholderStyle?: string,
+    divStyle?: string,
 }) => {
     // placeholders once set, cannot be changed
     const readonlyPlaceholder = useRef(placeholder);
     return (
-        <div className="flex flex-col gap-2">
-            <label htmlFor={id} className="font-semibold dark:text-white">
+        <div className={divStyle || "flex flex-col gap-2"}>
+            <label htmlFor={id} className={titleStyle || "font-semibold dark:text-white"}>
                 {title}
             </label>
             <input
@@ -30,7 +38,7 @@ const FormTextField = ({
                 value={value || ""}
                 placeholder={readonlyPlaceholder.current}
                 onChange={(e) => setValue(e.target.value)}
-                className={`
+                className={placeholderStyle || `
                 py-3 px-4 rounded-t-md bg-transparent transition-colors duration-200 font-medium
                 border-b-2 border-zinc-600 focus:outline-none
                 hover:border-b-emerald-400
