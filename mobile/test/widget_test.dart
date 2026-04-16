06ed4be0aded8d3781aaa67a229b4130c61df04c
Tales Souza Dias <fortune.8335@gmail.com>
2026-04-16 01:30:41 -0400

    update widget test for splash screen

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -5,6 +5,9 @@ import 'package:mobile/main.dart';
 void main() {
   testWidgets('Render Login', (tester) async {
     await tester.pumpWidget(const MyApp());
+    await tester.pumpAndSettle(Duration(seconds: 30));
+
+    await tester.tap(find.byType(MaterialApp));
     await tester.pumpAndSettle();
 
     expect(find.widgetWithText(ElevatedButton, 'Sign In'), findsOneWidget);
